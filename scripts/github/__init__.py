a78b125c7b4a5118c576f9c68816cf770d55c478
WuXiangM <1615372377@qq.com>
2026-07-14 16:46:38 +0800

    refactor(github scripts): 新增fetch_repo_readme导出
    将fetch_repo_readme函数添加到模块导出列表中，完善对外暴露的接口

--- scripts/github/__init__.py
+++ scripts/github/__init__.py
@@ -1,3 +1,3 @@
-from scripts.github.github_api import get_starred_repos
+from scripts.github.github_api import get_starred_repos, fetch_repo_readme
 
-__all__ = ["get_starred_repos"]
+__all__ = ["get_starred_repos", "fetch_repo_readme"]
